e3c53fc97f58fdf5dbe1b3eb00f7311487d9c42e
root <ngomfa@gmail.com>
2018-11-16 15:44:28 +0100

    exo3

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>PHP Partie1_Exo3</title>
+</head>
+<body>
+  <?php
+  $km = 1;
+  echo $km . " ";
+
+  $km = 3;
+  echo $km . " ";
+
+$km = 125;
+  echo $km;
+
+  ?>
+</body>
+</html>
